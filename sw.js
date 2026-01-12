ac031094dfc9d90fbea3e1636d1f90a1b0722946
djpridgenjr <djpridgenjr@gmail.com>
2026-01-11 23:15:37 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Offline cache (v2)
+const CACHE_NAME = "fb-mass-logbook-v6";
+const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
+
+
+self.addEventListener("message", (event) => {
+  if(event.data && event.data.type === "SKIP_WAITING"){
+    self.skipWaiting();
+  }
+});
